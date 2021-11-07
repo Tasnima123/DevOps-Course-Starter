9e306bea4a72407aa92c458738b56bc89350ab4d
Tasnima123 <Tasnima123@hotmail.co.uk>
2021-11-07 20:15:44 +0000

    Added logs

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -4,4 +4,5 @@ class Config:
     """Base configuration variables."""
     SECRET_KEY = os.environ.get('SECRET_KEY')
     LOG_LEVEL = os.environ.get('LOG_LEVEL')
+    LOGGLY_TOKEN = os.environ.get('LOGGLY_TOKEN')
     
