76cd0bf55f0b615ef0efec3fa98386c23658eb40
Tasnima123 <Tasnima123@hotmail.co.uk>
2021-11-11 00:10:25 +0000

    updated files

--- webhook.sh
+++ webhook.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 terraform init
+terraform apply -var="location=uksouth" -var="client_id=$client_id" -var="client_secret=$client_secret" -var="LOGGLY_TOKEN=$LOGGLY_TOKEN" -auto-approve
 docker push $DOCKER_USERNAME/my-test-image:latest
 terraform output -raw cd_webhook
